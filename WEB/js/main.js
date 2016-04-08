abacc80f012501d39b609c836d4f6a7ee8a5d084
Gabriel Melendez Ponce <gaby.melen.ponce@gmail.com>
2016-04-07 20:16:08 -0500

    Cambios y agregar sección misión

--- WEB/js/main.js
+++ WEB/js/main.js
@@ -1,16 +1,10 @@
   var aspect_ratio = 0.56;
   var $hero_video = $(".hero");
-  var $circulo_principal = $(".quienes .circulo");
   $(document).ready(function() {
     if ($(window).width() < 1100) {
       $hero_video.height($hero_video.width() * aspect_ratio);
     } else {
       $hero_video.height(620);
-    }
-    if ($(this).width() > 768) {
-      $(".quienes .circulo").height($(".imagen").height());
-    }else {
-      $(".quienes .circulo").height("auto");
     }
   });
   jQuery(window).resize(function() {
@@ -18,10 +12,5 @@
       $hero_video.height($hero_video.width() * aspect_ratio);
     } else {
       $hero_video.height(620);
-    }
-    if ($(this).width() > 768) {
-      $(".quienes .circulo").height($(".imagen").height());
-    }else {
-      $(".quienes .circulo").height("auto");
     }
   });
