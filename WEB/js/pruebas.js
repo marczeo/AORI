e31d6572c6cfa3052868bfe5e5db1e9b75bf971f
Gerardo Nille <gerardonille@gmail.com>
2016-04-08 13:59:17 -0500

    Animacion y un poco de estructura en index

---
+++ WEB/js/pruebas.js
@@ -0,0 +1,47 @@
+alert("fuera a la funcion");
+var $window           = $(window),
+win_height_padded = $window.height() * 1.1,
+isTouch           = Modernizr.touch;
+$window.on('scroll', revealOnScroll);
+ alert("fuera aasdasdcion");
+$(function() {
+ alert("fuera entro a la funcion dcion");
+  var $window           = $(window),
+      win_height_padded = $window.height() * 1.1,
+      isTouch           = Modernizr.touch;
+
+  if (isTouch) { $('.revealOnScroll').addClass('prueba'); }
+
+  $window.on('scroll', revealOnScroll);
+ alert("fuera aasdasdcion");
+  function revealOnScroll() {
+    var scrolled = $window.scrollTop(),
+        win_height_padded = $window.height() * 1.1;
+
+    // Showed...
+    $(".revealOnScroll:not(prueba)").each(function () {
+      var $this     = $(this),
+          offsetTop = $this.offset().top;
+ alert("fuera aasdasdcion");
+      if (scrolled + win_height_padded > offsetTop) {
+        if ($this.data('timeout')) {
+          window.setTimeout(function(){
+            $this.addClass('animated ' + $this.data('animation'));
+          }, parseInt($this.data('timeout'),10));
+        } else {
+          $this.addClass('animated ' + $this.data('animation'));
+        }
+      }
+    });
+    // Hidden...
+   $(".revealOnScroll.animated").each(function (index) {
+      var $this     = $(this),
+          offsetTop = $this.offset().top;
+      if (scrolled + win_height_padded < offsetTop) {
+        $(this).removeClass('animated fadeInUp flipInX lightSpeedIn')
+      }
+    });
+  }
+
+  revealOnScroll();
+});
