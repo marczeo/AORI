38ae34c49feaff5ef57a61f977fdc03093fb7ab6
marczeo <marco.gomez@outlook.com>
2016-04-11 14:15:43 -0500

    Contacto
    Validacion

--- WEB/js/contacto.js
+++ WEB/js/contacto.js
@@ -1,17 +1,40 @@
-
-
 $(document).ready(function() {
-  $(".contact-dropdown-services dt a").on('click', function() {
-  $(".contact-dropdown-services dd ul").slideToggle('fast');
+	//Combo con checkbox
+	$(".contact-dropdown-services dt a").on('click', function() {
+		$(".contact-dropdown-services dd ul").slideToggle('fast');
+	});
+
+	$(".contact-dropdown-services dd ul li a").on('click', function() {
+		$(".contact-dropdown-services dd ul").hide();
+	});
+	//Fin slide
+
+	//Validacion formulario contacto
+	$("#contact-form").submit(function(e) {
+
+    var url = "php/sendmail.php";
+
+    $.ajax({
+           type: "POST",
+           url: url,
+           data: $("#contact-form").serialize(), // serializes the form's elements.
+           success: function(data)
+           {
+           		if(data=="true")
+           		{
+           			alert"Mensaje enviado con exito");
+				}
+				else
+				{
+					alert("Debe llenar todos los campos");
+				}
+           }
+         });
+
+    e.preventDefault();
 });
-
-$(".contact-dropdown-services dd ul li a").on('click', function() {
-  $(".contact-dropdown-services dd ul").hide();
-});
-
-function getSelectedValue(id) {
-  return $("#" + id).find("dt a span.value").html();
-}
+	//Fin de validacion
 
 
+	
 });
