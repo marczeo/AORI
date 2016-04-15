60a331a66c05dce045edd3030b92edc01c924105
Gabriel Melendez Ponce <gaby.melen.ponce@gmail.com>
2016-04-14 19:46:32 -0500

    mapa y contacto

--- WEB/js/maps.js
+++ WEB/js/maps.js
@@ -21,11 +21,71 @@ window.onload = function() {
 };
 
 function init_map() {
+    var styles = [{
+    "featureType": "administrative",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+        "color": "#444444"
+    }]
+}, {
+    "featureType": "landscape",
+    "elementType": "all",
+    "stylers": [{
+        "color": "#f2f2f2"
+    }]
+}, {
+    "featureType": "poi",
+    "elementType": "all",
+    "stylers": [{
+        "visibility": "off"
+    }]
+}, {
+    "featureType": "poi",
+    "elementType": "labels.text",
+    "stylers": [{
+        "visibility": "off"
+    }]
+}, {
+    "featureType": "road",
+    "elementType": "all",
+    "stylers": [{
+        "saturation": -100
+    }, {
+        "lightness": 45
+    }]
+}, {
+    "featureType": "road.highway",
+    "elementType": "all",
+    "stylers": [{
+        "visibility": "simplified"
+    }]
+}, {
+    "featureType": "road.arterial",
+    "elementType": "labels.icon",
+    "stylers": [{
+        "visibility": "off"
+    }]
+}, {
+    "featureType": "transit",
+    "elementType": "all",
+    "stylers": [{
+        "visibility": "off"
+    }]
+}, {
+    "featureType": "water",
+    "elementType": "all",
+    "stylers": [{
+        "color": "#dbdbdb"
+    }, {
+        "visibility": "on"
+    }]
+}];
     var myOptions = {
         zoom: 6,
-				scrollwheel: false,
+        scrollwheel: false,
         center: new google.maps.LatLng(20.7018007, -103.3808581),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: styles
     };
     map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
     marker = new google.maps.Marker({
