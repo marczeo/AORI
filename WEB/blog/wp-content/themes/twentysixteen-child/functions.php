89279064018c98a3a9abcba65912e9c86b32ba43
Gabriel Melendez Ponce <gaby.melen.ponce@gmail.com>
2016-04-18 16:25:11 -0500

    masonry en Blog

--- WEB/blog/wp-content/themes/twentysixteen-child/functions.php
+++ WEB/blog/wp-content/themes/twentysixteen-child/functions.php
@@ -1,7 +1,20 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
-}
+  function theme_enqueue_jquery() {
+    wp_enqueue_script('jquery');
+  }
+  add_action('wp_enqueue_script','theme_enqueue_jquery');
+  add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+  function theme_enqueue_styles() {
+      wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+  }
+  function masonry_enqueue_scripts() {
+    	wp_register_script('masonry', plugins_url('masonry.pkgd.min.js', __FILE__), array('jquery'),'1.1', true);
+      wp_enqueue_script( 'masonry');
+  }
+  /*Controla la longitud de la previsualización del texto y lo limita a 20 palabras*/
+  add_action( 'wp_enqueue_scripts', 'masonry_enqueue_scripts' );
+    function new_excerpt_length($length) {
+  	return 20;
+  }
+  add_filter('excerpt_length', 'new_excerpt_length');;
 ?>
--- WEB/blog/wp-content/themes/twentysixteen/index.php
+++ WEB/blog/wp-content/themes/twentysixteen/index.php
@@ -18,42 +18,47 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<!--masonry grid dentro de posts-->
+			<div class="grid">
+				<!--Generador de columnas-->
+	  	<div class="grid-sizer"></div>
+			<?php if ( have_posts() ) : ?>
 
-		<?php if ( have_posts() ) : ?>
+				<?php if ( is_home() && ! is_front_page() ) : ?>
+					<header>
+						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					</header>
+				<?php endif; ?>
 
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-			<?php endif; ?>
+				<?php
+				// Start the loop.
+				while ( have_posts() ) : the_post();
 
-			<?php
-			// Start the loop.
-			while ( have_posts() ) : the_post();
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', get_post_format() );
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+				// End the loop.
+				endwhile;
 
-			// End the loop.
-			endwhile;
+				// Previous/next page navigation.
+				the_posts_pagination( array(
+					'prev_text'          => __( 'Previous page', 'twentysixteen' ),
+					'next_text'          => __( 'Next page', 'twentysixteen' ),
+					'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>',
+				) );
 
-			// Previous/next page navigation.
-			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentysixteen' ),
-				'next_text'          => __( 'Next page', 'twentysixteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>',
-			) );
+			// If no content, include the "No posts found" template.
+			else :
+				get_template_part( 'template-parts/content', 'none' );
 
-		// If no content, include the "No posts found" template.
-		else :
-			get_template_part( 'template-parts/content', 'none' );
+			endif;
+			?>
 
-		endif;
-		?>
+		</div>
 
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
