ef52ac4628f4c880ec87a639672f71a33662093e
Gabriel Melendez Ponce <gaby.melen.ponce@gmail.com>
2016-04-18 17:30:46 -0500

    acomodos del blog

--- WEB/blog/wp-content/themes/twentysixteen-child/template-parts/content.php
+++ WEB/blog/wp-content/themes/twentysixteen-child/template-parts/content.php
@@ -65,12 +65,10 @@ jQuery(document).ready(function (){
   var $grid = jQuery('.grid').imagesLoaded( function() {
   // init Masonry after all images have loaded
   $grid.masonry({
-  // set itemSelector so .grid-sizer is not used in layout
-  itemSelector: '.grid-item',
-  // use element for option
-  columnWidth: '.grid-sizer',
-  gutter: 5,
-  percentPosition: true
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    gutter: 10,
+    percentPosition: true
   });
 });
 
