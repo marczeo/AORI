4be7ea8d3b278ff5739161c8095def9ab87891c7
Gabriel Melendez Ponce <gaby.melen.ponce@gmail.com>
2016-04-18 19:40:52 -0500

    optimización de archivos para servicios

--- WEB/js/servicios.js
+++ WEB/js/servicios.js
@@ -1,26 +1,10 @@
-
-  window.onload = function() {
-    //init_map();
-   /* $('.svg').click(function() {
-		//alert("hola marco");
-       alert($(this).attr('id'));
-	if ($("."+$(this).attr('id')).is(':empty')){
-		alert("Esta vacio empty");
-  $( "."+$(event.target).attr('id') ).load( $(event.target).attr('id') + ".html" );
-	}
-	else{
-  $(this).html("");
-	}
-return false;
-
-    });*/
-	$page_elements = $('.svg');
-	$($page_elements).each(function() {
-			//alert($(this).attr('id'));
-			$( "."+$(this).attr('id') ).load( $(this).attr('id') + ".html" );
-        });
+window.onload = function() {
+    $page_elements = $('.svg');
+    $page_elements.each(function() {
+        $("." + $(this).attr('id')).load($(this).attr('id') + ".html");
+    });
     $('.svg').click(function() {
-		$( "."+$(this).attr('id') ).slideToggle("slow",function(){
-		});
-	});
+        $("." + $(this).attr('id')).slideToggle();
+        return 0;
+    });
 };
