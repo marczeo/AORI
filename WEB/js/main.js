b67e9d32cb0f21c92cb1f7096a435a40c911f223
Gabriel Melendez Ponce <gaby.melen.ponce@gmail.com>
2016-04-18 20:04:59 -0500

    cambio en servicios

--- WEB/js/main.js
+++ WEB/js/main.js
@@ -2,35 +2,33 @@
   var $hero_video = $(".hero");
   $(document).ready(function() {
 
-    if ($(window).width() < 1100) {
-      $hero_video.height($hero_video.width() * aspect_ratio);
+      if ($(window).width() < 1100) {
+          $hero_video.height($hero_video.width() * aspect_ratio);
 
-    } else {
-      $hero_video.height(620);
-    }
-    //Incluir navbar
-    $(function(){
-      $("#navbar").load("navbar.html");
-    });
-    loadPage();
+      } else {
+          $hero_video.height(620);
+      }
+      //Incluir navbar
+      $(function() {
+          $("#navbar").load("navbar.html");
+      });
+      loadPage();
   });
   jQuery(window).resize(function() {
-    if ($(this).width() < 1100) {
-      $hero_video.height($hero_video.width() * aspect_ratio);
-    } else {
-      $hero_video.height(620);
-    }
+      if ($(this).width() < 1100) {
+          $hero_video.height($hero_video.width() * aspect_ratio);
+      } else {
+          $hero_video.height(620);
+      }
   });
 
-function loadPage()
-{
-  var url=window.location.href;
-  var hayAncla = url.indexOf("#");
+  function loadPage() {
+      var url = window.location.href;
+      var hayAncla = url.indexOf("#");
 
-  if(hayAncla!=-1)
-  {
-    var hash = url.substring(url.indexOf("#")+1);
-    window.location.hash=hash;
+      if (hayAncla != -1) {
+          var hash = url.substring(url.indexOf("#") + 1);
+          window.location.hash = hash;
+      }
+
   }
-
-}
--- WEB/js/pruebas.js
+++ WEB/js/pruebas.js
@@ -1,47 +1,50 @@
 alert("fuera a la funcion");
-var $window           = $(window),
-win_height_padded = $window.height() * 1.1,
-isTouch           = Modernizr.touch;
+var $window = $(window),
+    win_height_padded = $window.height() * 1.1,
+    isTouch = Modernizr.touch;
 $window.on('scroll', revealOnScroll);
- alert("fuera aasdasdcion");
+alert("fuera aasdasdcion");
 $(function() {
- alert("fuera entro a la funcion dcion");
-  var $window           = $(window),
-      win_height_padded = $window.height() * 1.1,
-      isTouch           = Modernizr.touch;
+    alert("fuera entro a la funcion dcion");
+    var $window = $(window),
+        win_height_padded = $window.height() * 1.1,
+        isTouch = Modernizr.touch;
 
-  if (isTouch) { $('.revealOnScroll').addClass('prueba'); }
+    if (isTouch) {
+        $('.revealOnScroll').addClass('prueba');
+    }
 
-  $window.on('scroll', revealOnScroll);
- alert("fuera aasdasdcion");
-  function revealOnScroll() {
-    var scrolled = $window.scrollTop(),
-        win_height_padded = $window.height() * 1.1;
+    $window.on('scroll', revealOnScroll);
+    alert("fuera aasdasdcion");
 
-    // Showed...
-    $(".revealOnScroll:not(prueba)").each(function () {
-      var $this     = $(this),
-          offsetTop = $this.offset().top;
- alert("fuera aasdasdcion");
-      if (scrolled + win_height_padded > offsetTop) {
-        if ($this.data('timeout')) {
-          window.setTimeout(function(){
-            $this.addClass('animated ' + $this.data('animation'));
-          }, parseInt($this.data('timeout'),10));
-        } else {
-          $this.addClass('animated ' + $this.data('animation'));
-        }
-      }
-    });
-    // Hidden...
-   $(".revealOnScroll.animated").each(function (index) {
-      var $this     = $(this),
-          offsetTop = $this.offset().top;
-      if (scrolled + win_height_padded < offsetTop) {
-        $(this).removeClass('animated fadeInUp flipInX lightSpeedIn')
-      }
-    });
-  }
+    function revealOnScroll() {
+        var scrolled = $window.scrollTop(),
+            win_height_padded = $window.height() * 1.1;
 
-  revealOnScroll();
+        // Showed...
+        $(".revealOnScroll:not(prueba)").each(function() {
+            var $this = $(this),
+                offsetTop = $this.offset().top;
+            alert("fuera aasdasdcion");
+            if (scrolled + win_height_padded > offsetTop) {
+                if ($this.data('timeout')) {
+                    window.setTimeout(function() {
+                        $this.addClass('animated ' + $this.data('animation'));
+                    }, parseInt($this.data('timeout'), 10));
+                } else {
+                    $this.addClass('animated ' + $this.data('animation'));
+                }
+            }
+        });
+        // Hidden...
+        $(".revealOnScroll.animated").each(function(index) {
+            var $this = $(this),
+                offsetTop = $this.offset().top;
+            if (scrolled + win_height_padded < offsetTop) {
+                $(this).removeClass('animated fadeInUp flipInX lightSpeedIn')
+            }
+        });
+    }
+
+    revealOnScroll();
 });
