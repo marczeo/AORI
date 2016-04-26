c27ea6109547b48370ba0d82614c81e28d69b6d1
Gabriel Melendez Ponce <gaby.melen.ponce@gmail.com>
2016-04-25 19:24:39 -0500

    agregar contenedor a servicios

--- WEB/js/scroll.js
+++ WEB/js/scroll.js
@@ -11,24 +11,24 @@ function check_if_in_view() {
         var element_bottom_position = (element_top_position + element_height);
         var resta = Math.abs((element_top_position - window_bottom_position));
         var resta2 = Math.abs((element_bottom_position - window_top_position));
-      
+
         if ((element_bottom_position >= window_top_position) &&
             (element_top_position <= window_bottom_position)) {
 
             $element.display = "inline";
-            
+
         } else if (Math.abs((element_top_position - window_bottom_position)) <= 40 || Math.abs((element_bottom_position - window_top_position)) <= 40) {
 
-			el  = $(this);
-			newone = el.clone(true),
-			el.before(newone);
+            el = $(this);
+            newone = el.clone(true),
+                el.before(newone);
 
-			var count = $('.prueba'+el.attr("id")).length;
-			
-			if(count > 1){
-				
-			$(".prueba"+el.attr("id") + ":last").remove();
-			}
+            var count = $('.prueba' + el.attr("id")).length;
+
+            if (count > 1) {
+
+                $(".prueba" + el.attr("id") + ":last").remove();
+            }
         }
     });
 }
